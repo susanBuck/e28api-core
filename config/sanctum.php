9f359c454ed437bf331ae74abb5d02aae90617af
Susan Buck <susan@susanbuck.net>
2020-11-23 14:24:12 -0500

    Default stateful

--- config/sanctum.php
+++ config/sanctum.php
@@ -12,12 +12,10 @@ return [
     | and production domains which access your API via a frontend SPA.
     |
     */
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', implode(
-        ',',
-        [
-    'localhost',
-    'localhost:8000']
-    ))),
+    'stateful' => explode(',', env(
+        'SANCTUM_STATEFUL_DOMAINS',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
+    )),
     
     /*
     |--------------------------------------------------------------------------
