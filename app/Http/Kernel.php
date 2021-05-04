febb4c76cdcc5643910949f3e0092dbb32dca00f
Susan Buck <susan@susanbuck.net>
2021-05-04 18:45:10 -0400

    Increase throttling

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             EnsureFrontendRequestsAreStateful::class,
-            'throttle:60,1',
+            'throttle:120,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
         ],
